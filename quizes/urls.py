b8ad3d9122ba2e5ea752dc26d7e6f4f74c505e2d
abhi-54 <71889182+abhi-54@users.noreply.github.com>
2021-12-04 17:48:26 +0530

    reupload

--- quizes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import (QuizListView, quiz_view)
-
-app_name = 'quizes'
-
-urlpatterns = [
-  path('', QuizListView.as_view(), name='main-view'),
-  path('<pk>/', quiz_view, name='quiz-view'),
-]
