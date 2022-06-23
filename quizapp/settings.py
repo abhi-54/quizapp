33c5be2fb6d9b86966a6a25c9761292e787628a2
abhi-54 <71889182+abhi-54@users.noreply.github.com>
2022-06-23 10:50:09 +0530

    all css

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 from questions.s_copy import S as error
 import django_heroku
-import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
